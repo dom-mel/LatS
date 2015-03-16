ece1625a2a144d87986c1338d59666765442907b
Marcus Pankalla <mpankalla@gmx.de>
2015-03-16 19:11:33 +0100

    More meaningful implementation of the CPU sensor

--- src/Dommel/LatsBundle/Sensors/CPU.php
+++ src/Dommel/LatsBundle/Sensors/CPU.php
@@ -9,7 +9,8 @@ use Dommel\LatsBundle\Services\Sensor;
 class CPU implements Sensor {
 
     /**
-     * readout information
+     * Get CPU usage in percent
+     * Ripped from http://stackoverflow.com/questions/13131003/get-cpu-percent-usage-in-php
      *
      * @param array $config configuration for the sensor from sensors.yml
      * @return SensedValue
@@ -19,11 +20,23 @@ class CPU implements Sensor {
     {
         $result = 0;
         
-        if (is_callable('sys_getloadavg')) {
-            $values = sys_getloadavg();
-            $result = $values[0];
+        $stat1 = file('/proc/stat'); 
+        sleep(1); 
+        $stat2 = file('/proc/stat'); 
+        $info1 = explode(" ", preg_replace("!cpu +!", "", $stat1[0])); 
+        $info2 = explode(" ", preg_replace("!cpu +!", "", $stat2[0])); 
+        $dif = array(); 
+        $dif['user'] = $info2[0] - $info1[0]; 
+        $dif['nice'] = $info2[1] - $info1[1]; 
+        $dif['sys'] = $info2[2] - $info1[2]; 
+        $dif['idle'] = $info2[3] - $info1[3]; 
+        $total = array_sum($dif); 
+        $cpu = array(); 
+        foreach($dif as $x=>$y) {
+            $cpu[$x] = round($y / $total * 100, 1);
         }
         
+        $result = $cpu[$config['stat']];
         
         return new SensedValue($result, SensedValue::TYPE_FLOAT);
     }
